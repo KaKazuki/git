3c8666c67adce2ed7eabf4c15af96cdd914670de
KaKazuki <k614kanamorik@gmail.com>
2022-05-21 22:35:17 +0900

    create App.vue

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>develop?</p>
+        <a href=""></a>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
